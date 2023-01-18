d58b481ee9f134d94912b459d33ab950b7c7cd84
vibha mishra <vkmmishra871@gmail.com>
2023-01-18 13:17:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css'
+
+import Form from './Form';
+  
+
+
+const App = () => {
+  
+  return (
+    <>
+    <div className='App'>
+        <h1>FORM BOX</h1>
+        <Form></Form>
+    </div>
+    </>
+  );
+}
+export default App;
---
+++ src/Form.js
@@ -0,0 +1,105 @@
+import { React, useState, useEffect } from "react";
+import "./form.css";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+// import { InputLabel } from "@mui/material";
+// import RaisedButton from "@mui/material/Raised"
+import Button from "@mui/material/Button";
+
+const stylee = {
+  outerbox: {
+    width: "50%",
+    minHeight: "max-content",
+    backgroundColor: "white",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+    textAlign: "center",
+    borderRadius: "10px",
+    boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+  },
+};
+
+const Form = () => {
+  const handleSubmit = () => {};
+
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [imageUrl, setImageUrl] = useState(null);
+
+  useEffect(() => {
+    if (selectedImage) {
+      setImageUrl(URL.createObjectURL(selectedImage));
+    }
+  }, [selectedImage]);
+
+  return (
+    <div style={stylee.outerbox}>
+      <form onSubmit={handleSubmit}>
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 1.5, width: "90%" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            id="standard-basic"
+            label="First Name"
+            variant="standard"
+            required
+          />
+          <TextField id="standard-basic" label="Last Name" variant="standard" required/>
+          <TextField
+            id="standard-basic"
+            label="Enter Your Address"
+            variant="standard"
+          />
+          <TextField
+            id="standard-basic"
+            label="Contact Number"
+            variant="standard"
+          />
+          {/* <div className="upload-input">
+                <InputLabel style={{paddingBottom:'10px'}}>Upload Image</InputLabel>
+                <input id="choose image/video" type="file" />
+          </div> */}
+
+          <input
+            accept="image/*"
+            type="file"
+            id="select-image"
+            style={{ display: "none" }}
+            onChange={(e) => setSelectedImage(e.target.files[0])}
+          />
+          <label htmlFor="select-image">
+            <Button variant="contained" color="primary" component="span">
+              Upload Image
+            </Button>
+          </label>
+          {imageUrl && selectedImage && (
+            <Box mt={2} textAlign="center">
+              <div>Image Preview:</div>
+              <img src={imageUrl} alt={selectedImage.name} height="100px" />
+            </Box>
+          )}
+
+
+
+          <TextField
+            id="standard-textarea"
+            label="Discription"
+            placeholder="Enter text here!"
+            multiline
+            variant="standard"
+            rows={3}
+          />
+        <Button type="submit" variant="contained" color="primary" component="span" style={{width:'90%'}}>Submit Form</Button>
+
+        </Box>
+      </form>
+    </div>
+  );
+};
+export default Form;
